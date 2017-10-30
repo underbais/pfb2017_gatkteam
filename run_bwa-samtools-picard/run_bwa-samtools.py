e14d6759291c2459311d875a1ed1c9b038ae56a1
bauchetg <bauchetg@gmail.com>
2017-10-30 00:02:24 +0000

    add picard MD function

--- run_bwa-samtools-picard/run_bwa-samtools.py
+++ run_bwa-samtools-picard/run_bwa-samtools.py
@@ -26,7 +26,7 @@ def call_bwa(refgen, fastq_r1, fastq_r2, threads = 1, mark_split = "-M", bwa_out
             os.makedirs(bwa_output)
         out_file_name = fastq_r1.split("_")[0]
         file_out = "{}/{}/{}.sam".format(my_path,bwa_output,out_file_name)
-        print(file_out)
+        #print(file_out)
         log_out =  "{}/{}.log".format(my_path,bwa_output)
 
         #run bwa mem
@@ -47,7 +47,7 @@ def call_bwa(refgen, fastq_r1, fastq_r2, threads = 1, mark_split = "-M", bwa_out
 
         # check exception content
     except Exception as e:
-        print(str(e))
+        #print(str(e))
         execution = None
     return execution  # return the proc object inclunding binary output and error log
 
@@ -63,6 +63,7 @@ if filepath_bwa is None:
 #################
 
 input_sam = filepath_bwa
+#print(input_sam)
 sample_name = os.path.basename(input_sam)
 sample_name = os.path.splitext(sample_name)[0]
 #print(sample_name)
@@ -74,11 +75,12 @@ def call_samtools_sort(input_sam, threads = 1, output_format = "BAM", samtools_o
         # create samtools output folder
         if not os.path.exists(samtools_output):
             os.makedirs(samtools_output)       
-        file_out = "{}/{}.sam".format(my_path,samtools_output,sample_name)
+        file_out = "{}/{}/{}.sorted.bam".format(my_path,samtools_output,sample_name)
+        #print(file_out)
         log_out =  "{}/{}.log".format(my_path,samtools_output)
 
         # run samtools_sort
-        cmd = "samtools sort -@ {} {} -O {} -o {}/{}.sorted.bam".format(threads, input_sam, output_format, samtools_output, sample_name)
+        cmd = "samtools sort -@ {} {} -O {} -o {}".format(threads, input_sam, output_format, file_out)
         proc = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         #print(cmd)
         outs,errs = proc.communicate()
@@ -90,7 +92,7 @@ def call_samtools_sort(input_sam, threads = 1, output_format = "BAM", samtools_o
         execution = file_out
             
     except Exception as e2:
-        print(str(e2))
+        #print(str(e2))
         execution = None
     return execution 
 
@@ -99,8 +101,8 @@ filepath_samtool = call_samtools_sort(input_sam)
 if filepath_samtool is None:
     print('"something went wrong in samtools"')
     sys.exit(1)
-#print(filepath_samtool)
-sys.exit(0)
+print(filepath_samtool)
+#sys.exit(0)
 
 #### PICARD function
 ####################
@@ -111,23 +113,27 @@ input_bam = filepath_samtool
 sample_name = (os.path.basename(input_bam)).split('.')[0]
 #print(sample_name)
 
-def call_picard_mrkup(input_bam, picard_output = "picard_output"):
+#my_path = os.getcwd()
+#print('current_dir', dir_path)
+
+def call_picard_mrkup(input_bam, picard_output = "picard_output", my_path = "./"):
     execution = 0
     try:
 
         # create picard output folder
         if not os.path.exists(picard_output):
             os.makedirs(picard_output)
-            
-        file_out = "{}/{}/{}.mrkdup.sorted.bam.stdout".format(my_path,picard_output,sample_name)
-        print(file_out)
-        log_out =  "{}/{}.mrkdup.sorted.bam.stderr.log".format(my_path,picard_output)
+           
+        file_out = "{}/{}/{}.mrkdup.sorted.bam".format(my_path,picard_output,sample_name)
+        #print(file_out)
+        log_out =  "{}/picard.mrkdup.sorted.bam.log".format(my_path)
+        #print(log_out)
 
         # run samtools_sort
         picard_path = 'java -Djava.io.tmpdir=./ -jar /usr/local/anaconda/share/picard-2.14-0/picard.jar'
-        cmd = "{} MarkDuplicates I={} O={}/{}.mrkdup.sorted.bam M={}/{}.mrkdup_metrics.txt".format(picard_path, input_bam, picard_output, sample_name, picard_output, sample_name)
+        cmd = "{} MarkDuplicates I={} O={} M={}.metrics.txt".format(picard_path, input_bam, file_out, file_out)
         proc = subprocess.Popen(cmd, shell = True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(cmd)
+        #print(cmd)
         
         # get errors
         outs, errs = proc.communicate()
@@ -135,17 +141,21 @@ def call_picard_mrkup(input_bam, picard_output = "picard_output"):
         out_log = open(log_out, "w")
         out_log.write(out_errs)
         out_log.close()
+        execution = file_out
         
     except Exception as e3:
         print(str(e3))
-        #execution = None
-        return execution
+        execution = None
+    return execution
 
 # get function output and  error:
 filepath_picard_mrkup = call_picard_mrkup(input_bam)
 if filepath_picard_mrkup is None:
     print('"something went wrong in Picard_mark_duplicates"')
     sys.exit(1)
-    #print(filepath_samtool)
+    print(filepath_picard_mrkup)
 sys.exit(0)
             
+##### PICARD function ARRG
+##########################
+
